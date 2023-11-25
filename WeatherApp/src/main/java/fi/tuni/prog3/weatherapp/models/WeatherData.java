e207990260c32a2fb14781cb054ad1c3cde5b57b
Xuan-An Cao <an.cao@tuni.fi>
2023-11-25 18:10:02 +0200

    refactor: set git ignorecase to false

---
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/models/WeatherData.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.prog3.weatherapp.models;
+
+/**
+ *
+ * @author Ayman Khan
+ */
+public class WeatherData {
+    
+}
