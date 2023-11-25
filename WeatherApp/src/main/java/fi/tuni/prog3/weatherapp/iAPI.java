064a4f694382408a27f6039a7e1df5b7b3c774db
Xuan-An Cao <an.cao@tuni.fi>
2023-11-25 18:16:30 +0200

    revert: to original commit to fix naming issue

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/iAPI.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/iAPI.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package fi.tuni.prog3.weatherapp.interfaces;
+package fi.tuni.prog3.weatherapp;
 
 /**
  * Interface for extracting data from the OpenWeatherMap API.
