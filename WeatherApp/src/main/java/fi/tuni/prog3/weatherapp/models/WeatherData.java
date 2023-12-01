1e2971f45901dc2526bc054f95a71c5a24ebb51f
Ayman Khan <ayman.khan@tuni.fi>
2023-12-01 15:29:01 +0200

    Implemented WeatherDataService with JavaDOC

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/models/WeatherData.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/models/WeatherData.java
@@ -4,14 +4,10 @@ package fi.tuni.prog3.weatherapp.Models;
 /**
  * The {@code WeatherData} class represents weather information for a specific location.
  * It contains details about the current weather conditions and forecasts.
- * This class incorporates timestamped weather data and forecasts.
- * 
- * It is extended by {@link WeatherHourlyData} and {@link WeatherDailyData} classes to represent hourly and daily forecasts, respectively.
- * 
+ * This class incorporates timestamped current weather data, hourly and daily forecasts.
+ *  
  * @author Ayman Khan
  */
-
-import java.util.List;
 
 public class WeatherData {
     
@@ -21,51 +17,91 @@ public class WeatherData {
     private int temp;
     private int tempFeelsLike;
     private int windDir;
+    private int minTemp;
+    private int maxTemp;
     private float windSpeed;   
     private float precipitation;
     private float lat;
     private float lon;
+    private float precipitationPerc;
     private String weatherDesc;
     private String icon;
-    private List<WeatherHourlyData> hourlyForecast;
-    private List<WeatherDailyData> dailyForecast;
 
     /**
      * Constructs a {@code WeatherData} object containing weather information.
-     * 
+     *       
      * @param timestamp         The timestamp representing the current time in Unix format (UTC).
-     * @param timeOffset        The time offset in seconds from UTC.
-     * @param temp              The temperature in Kelvin.
-     * @param tempFeelsLike     The perceived temperature in Kelvin.
-     * @param windDir           The wind direction in degrees.
      * @param windSpeed         The wind speed in meters per second.
+     * @param icon              The icon code representing the weather condition.
+     */
+    
+    public WeatherData(int timestamp, float windSpeed, String icon) {
+        this.timestamp = timestamp;
+        this.windSpeed = windSpeed;
+        this.icon = icon;
+    }
+    
+    /**
+     * Constructs a {@code WeatherData} object containing current weather information.
+     * 
+     * @param temp              The temperature in Kelvin.
+     * @param windSpeed         The wind speed in meters per second.
+     * @param icon              The icon code representing the weather condition.
+     * @param timestamp         The timestamp representing the current time in Unix format (UTC).
+     * @param timeOffset        The time offset in seconds from UTC.    
+     * @param tempFeelsLike     The perceived temperature in Kelvin.
+     * @param windDir           The wind direction in degrees.   
      * @param precipitation     The intensity of precipitation in millimeters.
      * @param lat               The latitude of the measured location.
      * @param lon               The longitude of the measured location.
-     * @param weatherDesc       A brief description of the weather condition.
-     * @param icon              The icon code representing the weather condition.
-     * @param hourlyForecast    A list of hourly forecasts ({@link WeatherHourlyData}).
-     * @param dailyForecast     A list of daily forecasts ({@link WeatherDailyData}).
+     * @param weatherDesc       A brief description of the weather condition.   
      */
+    
     public WeatherData(int timestamp, int timeOffset, int temp, int tempFeelsLike, int windDir,
                        float windSpeed, float precipitation, float lat, float lon,
-                       String weatherDesc, String icon, List<WeatherHourlyData> hourlyForecast,
-                       List<WeatherDailyData> dailyForecast) {
-        this.timestamp = timestamp;
-        this.timeOffset = timeOffset;
+                       String weatherDesc, String icon) {
+        this(timestamp, windSpeed, icon);
         this.temp = temp;
+        this.timeOffset = timeOffset;
         this.tempFeelsLike = tempFeelsLike;
         this.windDir = windDir;
-        this.windSpeed = windSpeed;
         this.precipitation = precipitation;
         this.lat = lat;
         this.lon = lon;
         this.weatherDesc = weatherDesc;
-        this.icon = icon;
-        this.hourlyForecast = hourlyForecast;
-        this.dailyForecast = dailyForecast;
     }
       
+    /**
+     * Constructs a {@code WeatherData} object containing hourly weather information.
+     * @param timestamp             The time in Unix format (UTC).
+     * @param temp                  The temperature in Kelvin.
+     * @param windSpeed             The wind speed in meters per second.
+     * @param icon                  The icon code representing the weather condition.
+     * @param precipitationPerc     The probability of precipitation.
+     */
+    public WeatherData(int timestamp, int temp, float windSpeed, String icon, float precipitationPerc) {
+        this(timestamp, windSpeed, icon);
+        this.temp = temp;
+        this.precipitationPerc = precipitationPerc;
+    } 
+    
+    /**
+     * Constructs a {@code WeatherData} object for daily weather forecasts.
+     * 
+     * @param timestamp             The time in Unix format (UTC).
+     * @param windSpeed             The wind speed in meters per second.
+     * @param icon                  The icon code representing the weather condition.
+     * @param precipitationPerc     The probability of precipitation.
+     * @param minTemp               The minimum temperature of the day in Kelvin.
+     * @param maxTemp               The maximum temperature of the day in Kelvin.
+     */
+    public WeatherData(int timestamp, float windSpeed, String icon, float precipitationPerc, int minTemp, int maxTemp) {
+        this(timestamp, windSpeed, icon);
+        this.precipitationPerc = precipitationPerc;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+    }
+    
     // GETTERS for WeatherData Feilds
     
     public int getTimestamp() {
@@ -111,126 +147,11 @@ public class WeatherData {
     public String getIcon() {
         return icon;
     }
-
-    public List<WeatherHourlyData> getHourlyForecast() {
-        return hourlyForecast;
-    }
-
-    public List<WeatherDailyData> getDailyForecast() {
-        return dailyForecast;
-    }
     
-}
-
-/**
- * The {@code WeatherHourlyData} class extends {@link WeatherData} and represents hourly weather forecasts.
- * It contains specific details for hourly weather predictions.
- * 
- * @author Ayman
- */
-class WeatherHourlyData extends WeatherData {
-
-    // Feilds for hourly forecasts
-    private int timestamp;
-    private int temp;
-    private float windSpeed;
-    private String icon;
-    private float precipitationPerc;
-
-    /**
-     * Constructs a {@code WeatherHourlyData} object for hourly weather forecasts.
-     * 
-     * @param timestamp             The time in Unix format (UTC).
-     * @param temp                  The temperature in Kelvin.
-     * @param windSpeed             The wind speed in meters per second.
-     * @param icon                  The icon code representing the weather condition.
-     * @param precipitationPerc     The probability of precipitation.
-     */
-    public WeatherHourlyData(int timestamp, int temp, float windSpeed, String icon, float precipitationPerc) {
-        super(0, 0, 0, 0, 0, 0, 0, 0, 0, "", "", null, null);
-        this.timestamp = timestamp;
-        this.temp = temp;
-        this.windSpeed = windSpeed;
-        this.icon = icon;
-        this.precipitationPerc = precipitationPerc;
-    }
-
-    // GETTERS for WeatherHourlyData Feilds
-    
-    public int getTimestamp() {
-        return timestamp;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public float getWindSpeed() {
-        return windSpeed;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
     public float getPrecipitationPerc() {
         return precipitationPerc;
     }
-}
-
-/**
- * The {@code WeatherDailyData} class extends {@link WeatherData} and represents daily weather forecasts.
- * It contains specific details for daily weather predictions.
- * 
- * @author Ayman
- */
-class WeatherDailyData extends WeatherData {
-
-    // Feilds for daily forecasts
-    private int timestamp;
-    private float windSpeed;
-    private String icon;
-    private float precipitationPerc;
-    private int minTemp;
-    private int maxTemp;
-
-    /**
-     * Constructs a {@code WeatherDailyData} object for daily weather forecasts.
-     * 
-     * @param timestamp             The time in Unix format (UTC).
-     * @param windSpeed             The wind speed in meters per second.
-     * @param icon                  The icon code representing the weather condition.
-     * @param precipitationPerc     The probability of precipitation.
-     * @param minTemp               The minimum temperature of the day in Kelvin.
-     * @param maxTemp               The maximum temperature of the day in Kelvin.
-     */
-    public WeatherDailyData(int timestamp, float windSpeed, String icon, float precipitationPerc, int minTemp, int maxTemp) {
-        super(0, 0, 0, 0, 0, 0, 0, 0, 0, "", "", null, null);
-        this.timestamp = timestamp;
-        this.windSpeed = windSpeed;
-        this.icon = icon;
-        this.precipitationPerc = precipitationPerc;
-        this.minTemp = minTemp;
-        this.maxTemp = maxTemp;
-    }
     
-    // GETTERS for WeatherDailyData Feilds
-    public int getTimestamp() {
-        return timestamp;
-    }
-
-    public float getWindSpeed() {
-        return windSpeed;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public float getPrecipitationPerc() {
-        return precipitationPerc;
-    }
-
     public int getMinTemp() {
         return minTemp;
     }
@@ -238,5 +159,5 @@ class WeatherDailyData extends WeatherData {
     public int getMaxTemp() {
         return maxTemp;
     }
-    
 }
+
--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/services/WeatherDataService.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/services/WeatherDataService.java
@@ -1,13 +1,199 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package fi.tuni.prog3.weatherapp.services;
 
 /**
- *
+ * The {@code WeatherDataService} class fetches weather data from the OpenWeatherMap API for a specific location.
+ * It retrieves current weather, 3-hourly forecasts for 5 days, and a 7-day daily forecast.
+ * This class contains methods to obtain weather information in different time intervals and formats.
+ * 
  * @author Ayman Khan
  */
+
+import fi.tuni.prog3.weatherapp.Models.WeatherData;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class WeatherDataService {
     
+    // Constants for API access
+    private static final String baseUrl = "https://api.openweathermap.org/data/2.5/";
+    private static final String apiKey = "d3f1720d73d8b9dbf2a82558b1d79103";
+    
+    // Default constructor
+    public WeatherDataService(){}
+      
+    /**
+     * Retrieves the current weather data for a specific location using latitude and longitude coordinates.
+     *
+     * @param lat Latitude of the location.
+     * @param lon Longitude of the location.
+     * @return WeatherData object representing the current weather conditions.
+     */
+    public WeatherData getCurrentWeatherData(float lat, float lon) {      
+        try
+        { 
+            String apiUrl = baseUrl + "weather?lat=" +lat+ "&lon=" +lon+ "&appid=" + apiKey;      
+            StringBuilder response = fetchDataRequest(apiUrl);
+            if (response != null && response.length() > 0) {
+                JSONObject json = new JSONObject(response.toString());
+                
+                int timestamp = json.getInt("dt");
+                float windSpeed = (float) json.getJSONObject("wind").getDouble("speed");
+                String icon = json.getJSONArray("weather").getJSONObject(0).getString("icon");
+
+                int temp = (int) json.getJSONObject("main").getDouble("temp");
+                int timeOffset = json.getInt("timezone");
+                int tempFeelsLike = (int) json.getJSONObject("main").getDouble("feels_like");
+                int windDir = json.getJSONObject("wind").getInt("deg");
+                float precipitation = json.optJSONObject("rain") != null ? json.getJSONObject("rain").optInt("1h") : 0;
+                float latValue = (float) json.getJSONObject("coord").getDouble("lat");
+                float lonValue = (float) json.getJSONObject("coord").getDouble("lon");
+                String weatherDesc = json.getJSONArray("weather").getJSONObject(0).getString("description");
+
+                WeatherData weatherData = new WeatherData(timestamp, timeOffset, temp, tempFeelsLike, windDir,
+                        windSpeed, precipitation, latValue, lonValue, weatherDesc, icon);
+
+                return weatherData;
+                
+            }       
+            else return null;
+        } catch (Exception e) {           
+            e.printStackTrace();
+            return null;
+        }   
+    }
+    
+    /**
+     * Retrieves the 3-hourly weather forecast for 5 days based on a location's latitude and longitude.
+     *
+     * @param lat Latitude of the location.
+     * @param lon Longitude of the location.
+     * @return ArrayList containing WeatherData objects representing 3-hourly forecasts.
+     */
+    public ArrayList<WeatherData> get5day3HourlyForecast(float lat, float lon) {
+        
+        ArrayList<WeatherData> _3HourlyWeatherForecast = new ArrayList<>();
+        
+        try
+        { 
+            String apiUrl = baseUrl + "forecast?lat=" +lat+ "&lon=" +lon+ "&appid=" + apiKey;      
+            StringBuilder response = fetchDataRequest(apiUrl);
+            if (response != null && response.length() > 0) {
+                
+                JSONObject json = new JSONObject(response.toString());
+                JSONArray forecastList = json.getJSONArray("list");
+            
+                for (int i = 0; i < forecastList.length(); i++) {
+                    JSONObject forecastObj = forecastList.getJSONObject(i);
+
+                    int timestamp = forecastObj.optInt("dt");
+                    float windSpeed = forecastObj.getJSONObject("wind").optFloat("speed");
+                    String icon = forecastObj.getJSONArray("weather").getJSONObject(0).optString("icon");
+                    int temp = forecastObj.getJSONObject("main").optInt("temp");
+                    float precipitationPerc = forecastObj.optJSONObject("rain") != null ?
+                            forecastObj.getJSONObject("rain").optInt("3h") : 0;
+
+                    WeatherData weatherData = new WeatherData(timestamp, temp, windSpeed, icon, precipitationPerc);
+                    _3HourlyWeatherForecast.add(weatherData);
+                }                      
+            }       
+        } catch (Exception e) {           
+            e.printStackTrace();
+        }  
+        
+        return _3HourlyWeatherForecast;
+    }
+    
+    /**
+     * Retrieves a 7-day daily weather forecast based on a location's latitude and longitude.
+     *
+     * @param lat Latitude of the location.
+     * @param lon Longitude of the location.
+     * @return ArrayList containing WeatherData objects representing daily forecasts.
+     */
+    public ArrayList<WeatherData> getWeeklyForecast(float lat, float lon) {
+        
+        ArrayList<WeatherData> weeklyWeatherForecast = new ArrayList<>();
+        
+        try
+        { 
+            String apiUrl = baseUrl + "forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=7&appid=" + apiKey;
+            StringBuilder response = fetchDataRequest(apiUrl);
+            if (response != null && response.length() > 0) {
+                
+                JSONObject json = new JSONObject(response.toString());              
+                JSONArray forecastList = json.getJSONArray("list");
+
+                for (int i = 0; i < forecastList.length(); i++) {
+                    JSONObject dailyForecast = forecastList.getJSONObject(i);
+
+                    int timestamp = dailyForecast.getInt("dt");
+                    float windSpeed = dailyForecast.getFloat("speed");
+                    String icon = dailyForecast.getJSONArray("weather").getJSONObject(0).getString("icon");
+                    float precipitationPerc = dailyForecast.getFloat("pop");
+                    int minTemp = (int) dailyForecast.getJSONObject("temp").getDouble("min");
+                    int maxTemp = (int) dailyForecast.getJSONObject("temp").getDouble("max");
+
+                    WeatherData weatherData = new WeatherData(timestamp, windSpeed, icon, precipitationPerc, minTemp, maxTemp);
+                    weeklyWeatherForecast.add(weatherData);
+                }                                  
+            }       
+        } catch (Exception e) {           
+            e.printStackTrace();
+        }  
+        
+        return weeklyWeatherForecast;
+    }    
+    
+    // HELPER FUNCTIONS
+    
+    private StringBuilder fetchDataRequest(String apiUrl) throws Exception{
+        
+        StringBuilder response = new StringBuilder();
+        System.out.println("Api URL: " + apiUrl); //DEBUGGING       
+        
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode); //DEBUGGING
+        
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                System.out.println("API Response: " + response); //DEBUGGING 
+        }
+        
+        return response;
+    }
+    
+    // Convert necessary WeatherData fields to expected system (metric/imperial)
+    public WeatherData convertToUnitSystem(WeatherData data, String system) {      
+        return null; 
+    }
+
+    // Returns local time in string format “dd.mm.yyyy hh:mm:ss”
+    public String getLocalTime(int timestamp, int timeOffset) {        
+        return null; 
+    }   
+    
+    // Return SVG string of wind direction with the given angle
+    public String getWindDirectionIcon(int windDir) {        
+        return null; 
+    }
 }
+
